8e4bce547674eb5f45ca8a17e0598d6aca3a4923
Michał Perlak <michalperlak@windowslive.com>
2017-07-20 21:58:33 +0200

    Exercise02

---
+++ kotlin-03/src/main/kotlin/Exercise03.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    print("What is the quote?")
+    val quote = readLine()
+    print("Who said it?")
+    val author = readLine()
+
+    println("$author says, \"$quote\"")
+}
